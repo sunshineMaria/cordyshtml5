3bfd121aa123b0b1ba87bb3c3ac487f6e45ac2b7
pkruysse@cordys.com <pkruysse@cordys.com@f061221c-096d-faed-b717-863d521ff404>
2012-04-10 12:15:39 +0000

    First commit

---
+++ trunk/cordys.mobile.js
@@ -0,0 +1,44 @@
+﻿if (typeof(jQuery) == "undefined") {
+	throw 'jQuery is required, please ensure it is loaded before this library'; 
+//	document.write('<script src="/cordys/html5/jquery/jquery-1.7.1.min.js"><\/script>');
+};
+if (!$.cordys) $.cordys = {};
+
+function getURLParameter(url, name) {
+	return (url.search.search( new RegExp("[\?\&]"+name+"=([^\&]*)") ) >= 0) ? RegExp.$1 : "";
+}
+function addURLParameter(url, name, value, skipEncode) {
+	var parSeparator = url.indexOf("?") < 0 ? "?" : "&";
+	url = url + parSeparator + name + "=" + (skipEncode ? value : encodeURIComponent(value));
+	return url;
+}
+function getCookie(cookieName)
+{
+	return (window.document.cookie.search( new RegExp("\\b("+cookieName+")=([^;]*)") ) >= 0) ? RegExp.$2 : "";
+}
+function deleteAllCookies()
+{
+	var cookies = document.cookie.split(";");
+	for(var i=0; i < cookies.length; ++i)
+	{
+		document.cookie = $.trim(cookies[i].split("=")[0]) + 
+				"=;expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/cordys";
+	}
+}
+
+function loginIntoCordys() {
+	$.mobile.changePage( "/cordys/html5/login.htm", { transition: "pop", changeHash: false } );
+}
+
+function loadScript(url, callback, async, cache) {
+	return $.ajax({
+		type: "GET",
+		url: url,
+		success: callback,
+		dataType: "script",
+		async: async || false,
+		cache: cache || true
+	});
+};
+
+loadScript("/cordys/html5/src/cordys.ajax.js");
