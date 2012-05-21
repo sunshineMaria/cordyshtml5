09a24cb188acc1e66e3ebc904114572fc2c8d1ec
csharbel@cordys.com <csharbel@cordys.com@f061221c-096d-faed-b717-863d521ff404>
2012-05-21 08:30:51 +0000

    1) Changes in model to support updates (insert, update, delete). 2) Some api's like revert, clear, addBusinessObject. 3) Sample page updated to showcase insert, update, delete.

--- trunk/src/cordys.model.js
+++ trunk/src/cordys.model.js
@@ -17,9 +17,14 @@
 
 	if (!$.cordys) $.cordys = {};
 	if (!$.cordys.ajax) loadScript("/cordys/html5/src/cordys.ajax.js");
+	// let us add the ko mapping plugin
+	if (typeof(ko) !== "undefined" && (! ko.mapping)) loadScript("/cordys/html5/knockout/knockout.mapping-latest.js");
+
 
 	$.cordys.model = function(settings) {
 		var self = this;
+
+		var opts = $.extend({}, $.cordys.model.defaults, settings);
 		this.objectName = settings.objectName;
 		if (typeof(ko) !== "undefined") {
 			this[this.objectName] = ko.observableArray();
@@ -28,10 +33,20 @@
 			this[this.objectName] = [];
 		}
 
+
 		this.readSettings = {
 			success : function(data) {
 				var objects = getObjects(data, self.objectName);
+				
 				if (typeof(self[self.objectName]) === "function") { // in case of knockout
+					// let us add observable for identifying changes
+					for (var objectKey in objects)
+					{
+						var object = objects[objectKey];
+						var observableObject = ko.mapping.fromJS(object);
+						addOptimisticLock(self, object, observableObject, false);
+						objects[objectKey] = observableObject;
+					} 
 					self[self.objectName](objects);
 					if (settings.read.success) {
 						settings.read.success(self[self.objectName]());
@@ -44,21 +59,218 @@
 				}
 			}
 		}
-		// Do the same for createSettings/updateSettings/deleteSettings
+		
+		this.updateSettings = {
+			async : false,
+			parameters : function (settings){
+				var updateContent = [];
+				self.objectsToBeUpdated = [];
+
+				if (typeof(self[self.objectName]) === "function") { // in case of knockout
+					var objects = self[self.objectName]();
+
+					if (objects){
+						for (var objectKey in objects){
+							var object = objects[objectKey];
+							// get xml content for changed objects
+							if (object.lock && object.lock.isDirty()){
+								self.objectsToBeUpdated.push(object);
+								// let us get the old bo from the saved state
+								var oldObject = object.lock.getInitialState();
+								// the objects here are Observavables, let us unmap to get the new bo
+								var newObject = ko.mapping.toJS(object);
+								// get the corresponding XML for the object and add it to the updateContent
+								updateContent.push($.cordys.json.js2xmlstring({tuple:{old:{Employees:oldObject}, 'new':{Employees:newObject}}}));
+							}
+						}
+					}
+
+				}
+				return updateContent.join("");
+			},
+
+			beforeSend: function (xhr, settings){
+				return self.objectsToBeUpdated.length > 0;
+			},
+
+			success: function (data){
+				// let us get the updated tuples if we are using tuple protocol	
+				if (opts.useTupleProtocol){
+					var updatedObjects =  $.map($.isArray(data.tuple) ? data.tuple : [data.tuple], function(tuple){
+							return getObjects(tuple['new'], self.objectName)
+						});
+				}
+				for (var count=0; count<self.objectsToBeUpdated.length; count++){
+					var objectBeforeUpdation = self.objectsToBeUpdated[count];
+					
+					if (opts.useTupleProtocol){
+						var objectAfterUpdation = updatedObjects[count];
+						// let us merge the updated object back in case of tuple protocol and update the lock to it
+						objectBeforeUpdation.lock._update(objectAfterUpdation);
+					}
+					else{
+						// other we update the lock to the latest state of the object
+						objectBeforeUpdation.lock._updateLock();
+					}
+				}
+
+			}
+		}
+
+		this.deleteSettings = {
+			async : false,
+            parameters : function (settings){
+				var deleteContent = [];
+				self.objectsToBeDeleted = [];
+				if (typeof(self[self.objectName]) === "function") { // in case of knockout
+					var objects = self[self.objectName]();
+					if (objects){
+						for (var objectKey in objects){
+							var object = objects[objectKey];
+							// find deleted objects
+							if (object._destroy === true){
+								self.objectsToBeDeleted.push(object);
+								// let us get the old bo from the initial saved state
+								var oldObject = object.lock.getInitialState();
+								// get the corresponding XML for the deleted object and add it to the deleteContent
+								deleteContent.push($.cordys.json.js2xmlstring({tuple:{old:{Employees:oldObject}}}));
+							}
+						}
+					}
+				}
+				return deleteContent.join("");
+			},
+			beforeSend: function (xhr, settings){
+				return self.objectsToBeDeleted.length > 0;
+			},
+
+			success: function (data){
+				// no special treatment for tuple protocol here. We just delete the objects
+				for (var count=0; count<self.objectsToBeDeleted.length; count++){
+					self[self.objectName].remove(self.objectsToBeDeleted[count]);
+				}
+			}
+		}
+
+			
+
+		this.createSettings = {
+			async : false,
+			parameters : function (settings){
+				var insertContent = [];
+				self.objectsToBeInserted = [];
+				if (typeof(self[self.objectName]) === "function") { // in case of knockout
+					var objects = self[self.objectName]();
+					if (objects){
+						for (var objectKey in objects){
+							var object = objects[objectKey];
+							// find deleted objects
+							if (! object.lock){
+								self.objectsToBeInserted.push(object);
+								// let us get the new bo by unwrapping the Observable
+								var newObject = ko.mapping.toJS(object);
+								// get the corresponding XML for the inserted object and add it to the insertContent
+								insertContent.push($.cordys.json.js2xmlstring({tuple:{'new':{Employees:newObject}}}));
+							}
+						}
+					}
+				}
+				return insertContent.join("");
+			},
+
+			beforeSend: function (settings){
+				return self.objectsToBeInserted.length > 0;
+			},
+
+			success: function (data){
+				// let us get the inserted tuples if we are using tuple protocol				
+				if (opts.useTupleProtocol){
+					var insertedObjects = getObjects(data, self.objectName);
+				}
+
+				for (var count=0; count<self.objectsToBeInserted.length; count++){
+					var objectBeforeInsertion = self.objectsToBeInserted[count];
+
+					if (opts.useTupleProtocol){
+						var objectAfterInsertion = insertedObjects[count];
+						// let us set the lock with the inserted object we get from the backend
+						addOptimisticLock(self, objectAfterInsertion, objectBeforeInsertion, false);
+						// let us also update the object with what we got from the backend
+						//?
+						objectBeforeInsertion.lock._update(objectAfterInsertion);
+					}
+					else{
+						// other we set the lock to the latest state of the object
+						addOptimisticLock(self, ko.mapping.toJS(objectBeforeInsertion), objectBeforeInsertion, false);
+					}
+				}
+			}
+		}
+
 
 		this.create = function(createSettings) {
-			$.cordys.ajax($.extend({}, settings.defaults, settings.create, self.createSettings, createSettings))
+			$.cordys.ajax($.extend({}, settings.defaults, settings.create, self.createSettings, createSettings));
 		};
+
 		this.read = function(readSettings) { 
-			$.cordys.ajax($.extend({}, settings.defaults, settings.read, self.readSettings, readSettings))
+			$.cordys.ajax($.extend({}, settings.defaults, settings.read, self.readSettings, readSettings));
 		};
+
 		this.update = function(updateSettings) { 
-			$.cordys.ajax($.extend({}, settings.defaults, settings.update, self.updateSettings, updateSettings))
+			$.cordys.ajax($.extend({}, settings.defaults, settings.update, self.updateSettings, updateSettings));
 		};
+
 		this['delete'] = function(deleteSettings) {
-			$.cordys.ajax($.extend({}, settings.defaults, settings['delete'], self.deleteSettings, deleteSettings))
+			$.cordys.ajax($.extend({}, settings.defaults, settings['delete'], self.deleteSettings, deleteSettings));
 		};
+
+
+		this.addBusinessObject = function(object){
+			if (! object) return;
+			if (! ko.isObservable(object)){
+				object =  ko.mapping.fromJS(object);
+			}
+			self[self.objectName].push(object);
+			return object;
+		}
+
+		this.revert = function() {
+			if (typeof(self[self.objectName]) === "function") { // in case of knockout
+				var objects = self[self.objectName]();
+
+				if (objects){
+					for (var objectKey in objects){
+						var object = objects[objectKey];
+						if (! object.lock){
+							// remove newly inserted objects
+							self[self.objectName].remove(object);
+						}
+						else {
+							 // revert the changed objects
+							 if (object.lock.isDirty()){
+								object.lock.undo();
+							 }
+							 // also unmark ones for deletion. Remember that changed ones could have also been marked for deletion
+							 if (object._destroy === true){
+								 // KO Observable array does not have a undestroy, so let us just remove the destroy flag 
+								 object.valueWillMutate();
+								 object._destroy = false;
+								 object.valueHasMutated();
+							 }
+						}
+					}
+				}
+			}
+		}
+
+		this.clear = function() {
+			self[self.objectName].removeAll();
+		}
 	};
+
+	$.cordys.model.defaults = {
+		useTupleProtocol: true
+	}
 
 	getObjects = function(obj, key, val) {
 		var objects = [];
@@ -82,26 +294,44 @@
 		}
 		return objects;
 	};
-	cordysOptimisticLock = function(data, isInitiallyDirty) {
+
+	addOptimisticLock = function(model, data, observableData, isInitiallyDirty) {
 		var result = function() {}
-		var _initialData = ko.observable(ko.toJSON(data));
+		var _initialState = data;
+		var _initialJSONString = ko.toJSON(observableData);
 		var _isInitiallyDirty = ko.observable(isInitiallyDirty);
 		
-		result.isDirty = ko.dependentObservable(function() {
-			return _isInitiallyDirty() || _initialData() !== ko.toJSON(data);
+		result.getInitialState = function() {
+			return _initialState;
+		}
+
+		result.isDirty = ko.computed({
+				read:function() {
+					return observableData && (_isInitiallyDirty() || _initialJSONString !== ko.toJSON(observableData));
+				},
+				deferEvaluation : true
 		});
 
-		result.reset = function() {
-			_initialData(ko.toJSON(data));
+		// Strictly to be used internally. Updates the lock to the current state of the object
+		result._updateLock = function(data) {
+			_initialState = data ? data : ko.mapping.toJS(observableData); 
+			_initialJSONString = ko.toJSON(observableData);
 			_isInitiallyDirty(false);
 		};
 
-		result.initialData = function() {
-			var idata = $.parseJSON(ko.utils.unwrapObservable(_initialData));
-			delete idata.__ko_mapping__;
-			return idata;
-		};
+		// Strictly to be used internally for updating the state and the lock after successful update/insert
+		result._update = function(newData) {
+			_initialState = newData;
+			ko.mapping.fromJS(newData, {}, observableData);
+			this._updateLock(newData);
+		}
 
+		result.undo = function() {
+			if (this.isDirty()){			
+				this._update(_initialState);
+			}
+		};
+		observableData.lock = result;
 		return result;
 	};
 
