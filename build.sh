5f9b95b0b85ee997c1e8184deacd6f8254e8ebfb
csharbel <csharbel@cordys.com>
2012-06-06 14:00:41 +0000

    Adding build.sh for triggering from Jenkins

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# build.sh for products
+
+if [ ! -n "$BCPSDK_HOME" ]
+then
+        echo 'Please set BCPSDK_HOME env variable pointing to sdk checkout'
+        exit 1
+fi
+
+export BUILD_HOME=$PWD/../..
+source $BUILD_HOME/setenv.sh
+
+"$JAVA_HOME/bin/java" $JAVA_ARGS -cp "$ANT_CP" org.apache.tools.ant.Main "-Droot.dir=$BUILD_HOME" "-Dsdk.dir=$BCPSDK_HOME" $*
+
+
+
