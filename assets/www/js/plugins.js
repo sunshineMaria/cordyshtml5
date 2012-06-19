f1d2977f1361f8394a2a4b7c7054dade4abe2771
theodoorvd <theodoorvd@gmail.com>
2012-06-19 11:35:37 +0000

    Added new functionality. 	- register a server without entering a password. 	- start the app from an external source. With a url.

--- assets/www/js/plugins.js
+++ assets/www/js/plugins.js
@@ -1,17 +1,5 @@
-// usage: log('inside coolFunc', this, arguments);
-// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function f(){ log.history = log.history || []; log.history.push(arguments); if(this.console) { var args = arguments, newarr; try { args.callee = f.caller } catch(e) {}; newarr = [].slice.call(args); if (typeof console.log === 'object') log.apply.call(console.log, console, newarr); else console.log.apply(console, newarr);}};
-
-// make it safe to use console.log always
-(function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
-(function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
-
-
-// place any jQuery/helper plugins in here, instead of separate, slower script files.
-
 (function($) {
 	
-
 	$.event.special.ihold = {
 		setup: function(data, namespaces, eventHandle) {
 			
@@ -20,36 +8,9 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
 			
 		},
 		add: function(handleObj) {
-			// $(handleObj.selector)
-			
-			/*var startTime, endTime;
-			var gbMove = false;
-
-			$(this).on('touchstart mousedown', handleObj.selector, function(e) {
-				e.preventDefault();
-				startTime = new Date().getTime();
-				gbMove = false;
-			})
-				.on('touchmove', handleObj.selector, function(e) {
-					e.preventDefault();
-					gbMove = true;
-				})
-
-				.on('touchend mouseup mouseleave', handleObj.selector, function(e) {
-					e.preventDefault();
-					e.stopPropagation();
-					endTime = new Date().getTime();
-					if(!gbMove && (endTime-startTime) / 1000 > .75) {
-						handleObj.handler(e);
-					}
-				});*/
-			
-			$(this).hammer().on('hold', handleObj.handler);///*, 'li'*/, self.ui.serverList.longPress
+			$(this).hammer().on('hold', handleObj.handler);
 		},
 		remove: function(handleObj) {
-			// not implemented yet
-			//throw new Error('This method is not implemented yet.');
-			
 			$(this).hammer().off('hold', handleObj.handler);
 		},
 		_default: function(event) {
@@ -57,5 +18,4 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
 		}
 	};
 	
-	
-}) (jQuery)
+}) (jQuery);
--- src/com/cordys/app/CordysPhoneGapTestActivity.java
+++ src/com/cordys/app/CordysPhoneGapTestActivity.java
@@ -2,27 +2,26 @@ package com.cordys.app;
 
 import org.apache.cordova.DroidGap;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 public class CordysPhoneGapTestActivity extends DroidGap {
 	
-	/** Called when the activity is first created. */
+	/** 
+	 * Called when the activity is first created. 
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		String incommingPath = getIntent().getDataString();
+		Uri data = getIntent().getData();
 		
-		if (null != incommingPath)
-			super.loadUrl("file:///android_asset/www/index.html?url=" + incommingPath);
-		else
-			super.loadUrl("file:///android_asset/www/index.html");
+		String url = "file:///android_asset/www/index.html";
 		
+		url += null == data
+			? ""
+			: "?" + data.getQuery();
 		
-		
+		super.loadUrl(url);
 	}
-	
-	
-	
-	
 }
