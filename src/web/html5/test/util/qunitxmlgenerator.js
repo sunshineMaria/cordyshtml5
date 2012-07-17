761238d0e869bd9f2c99ad6dec0c7ab225301d5e
csharbel <csharbel@cordys.com>
2012-07-17 07:10:00 +0000

    Adding script file for running qunit files from phantomjs. Also added utility file for getting XML Results

---
+++ src/web/html5/test/util/qunitxmlgenerator.js
@@ -0,0 +1,47 @@
+function getTestResultXML() {
+    var outputXML = "<testsuites> <testsuite name=\"testsuite\">";
+
+    var testResultRoot = $("#qunit-tests");
+    var passedTestResults = testResultRoot.find(">li.pass,>li.fail");
+
+    // travers through the modules
+    $.each(passedTestResults, function (testIndex, testResult) {
+        outputXML += "<testcase ";
+
+        var moduleName = $(testResult).find("strong span.module-name").text();
+        var testName = $(testResult).find("strong span.test-name").text();
+
+
+        outputXML += " classname=\"" + moduleName + "\"";
+        outputXML += " name=\"" + testName + "\"";
+        outputXML += ">";
+
+        var passedTestCases = $(testResult).find(">ol>li.pass,ol>li.fail");
+
+        // travers through all the failed and succeeded tests
+        $.each(passedTestCases, function (testCaseIndex, testCase) {
+            var className = $(testCase).attr("class");
+            var testMessage = $(testCase).find(".test-message").text();
+            outputXML += (className === "pass") ? "<success" : "<failure";
+            outputXML += (className === "pass") ? " type =\"success\"" : " type =\"failed\"";
+            outputXML += " message =\"" + testMessage + "\"";
+            outputXML += "/>";
+
+            // add details in case of failures
+            if (className === "fail") {
+                var details = $(testCase).find("table").text();
+                outputXML += "<system-out>"
+                outputXML += details;
+                outputXML += "</system-out>"
+
+            }
+        });
+
+
+        outputXML += "</testcase>"
+
+    });
+    outputXML += "</testsuite></testsuites>";
+
+    return outputXML;
+}
