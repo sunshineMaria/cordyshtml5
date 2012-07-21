a3abbacd7135b57bacfc8d6f598ae72c5be5e3d1
kvinoth <kvinoth@cordys.com>
2012-07-21 08:56:07 +0000

    Included test for missing APIs

--- src/web/html5/test/test.cordys.case.js
+++ src/web/html5/test/test.cordys.case.js
@@ -312,5 +312,227 @@
 			start();
 		}, 3000);
 	});
+	
+	$.mockjax({
+		url: '*/com.eibus.web.soap.Gateway.wcp',
+		data: /PlanActivities/,
+		responseText: {
+		   activestates:{			  
+			  "@name": "Default State",
+			  "@id": "RootCaseModelState",
+			  "state": "001CC42E-BB14-11E1-FA48-F645A62E74AF"
+		   }
+		}
+	});
+	
+	test("Plan Activities", 3, function () {
+        stop();
+        $.cordys['case'].planActivities("001CC42E-BB14-11E1-FA48-F645A62E34AF", {
+				beforeSend:function(xhr, settings){
+					var planActivitiesRequest = "<SOAP:Envelope xmlns:SOAP='http://schemas.xmlsoap.org/soap/envelope/'><SOAP:Body><PlanActivities xmlns='http://schemas.cordys.com/casemanagement/execution/1.0'><caseinstanceid>001CC42E-BB14-11E1-FA48-F645A62E34AF</caseinstanceid></PlanActivities></SOAP:Body></SOAP:Envelope>";
+					equal(compareXML(planActivitiesRequest,settings.data), true, "Compare Request XML");
+				},
+				success: function(data) {
+					equal(data.activestates["@id"], "RootCaseModelState", "Plan Activity state id  RootCaseModelState");
+					equal(data.activestates.state, "001CC42E-BB14-11E1-FA48-F645A62E74AF", "Plan Activity state value 001CC42E-BB14-11E1-FA48-F645A62E74AF");
+					start();
+				},
+            error: function (jqXHR, errorStatus, errorThrown, errorCode, errorMessage) {
+                ok(false, "error found: " + errorMessage);
+                start();
+                return false;
+            }
+        })
+    });
+	
+	$.mockjax({
+		url: '*/com.eibus.web.soap.Gateway.wcp',
+		data: /GetFollowupActivities/,
+		responseText: {
+		   GetFollowupActivitiesResponse:{
+			  "@xmlns:SOAP":"http://schemas.xmlsoap.org/soap/envelope/",
+			  "@xmlns":"http://schemas.cordys.com/casemanagement/execution/1.0",
+			  "case:followups":{
+				 "case:followup":{
+					"@activity:id":"001CC42E-BB14-11E1-FA4E-358C103C74AF",
+					"@activityname":"Activity2",
+					"@activitytype":"HUMANTASK",
+					"@stateId":"RootCaseModelState",
+					"@stateName":"Default State",
+					"@type":"AUTOMATIC"
+					}
+				}	
+			}
+		}
+	});
+	
+	test("Get Followup Activities", 2, function () {
+        stop();
+        $.cordys['case'].getFollowupActivities("001CC42E-BB14-11E1-FA4E-3E43A945D4AF", {
+				beforeSend:function(xhr, settings){
+					var getFollowupActivitiesRequest = "<SOAP:Envelope xmlns:SOAP='http://schemas.xmlsoap.org/soap/envelope/'><SOAP:Body><GetFollowupActivities xmlns='http://schemas.cordys.com/casemanagement/execution/1.0'><caseinstanceid>001CC42E-BB14-11E1-FA4E-3E43A945D4AF</caseinstanceid></GetFollowupActivities></SOAP:Body></SOAP:Envelope>";
+					equal(compareXML(getFollowupActivitiesRequest,settings.data), true, "Compare Request XML");
+				},
+				success: function(data) {
+					equal(data.error, undefined, "Getting followup activities");
+					start();
+				},
+            error: function (jqXHR, errorStatus, errorThrown, errorCode, errorMessage) {
+                ok(false, "error found: " + errorMessage);
+                start();
+                return false;
+            }
+        })
+    });
+	
+	$.mockjax({
+		url: '*/com.eibus.web.soap.Gateway.wcp',
+		data: /GetActivityDefinition/,
+		responseText: {
+		   GetActivityDefinitionResponse:{
+			  "caseinstanceid":"001CC42E-BB14-11E1-FA4E-3E43A945D4AF",
+			  activities:{
+				 "case:activity":{
+					"@xmlns:ns1":"http://schemas.cordys.com/1.0/xforms/processapi",
+					"@xmlns:sm":"http://www.w3.org/2005/07/scxml",
+					"@xmlns:case":"http://schemas.cordys.com/casemanagement/1.0",
+					"@xmlns":"",
+					"@id":"001CC42E-BB14-11E1-FA4E-358C104134AF",
+					"@name":"Activity1",
+					"@type":"HUMANTASK",
+					HumanInteraction:{
+					   "@xmlns":"http://schemas.cordys.com/notification/workflow/1.0",
+					   "HumanTaskModelId":"001CC42E-BB14-11E1-FA4E-369AF76BF4AF",
+					   "Subject":"Activity1",
+					   "SendTo":""
+					},
+					"case:messages":"",
+					"case:associatedevents":"",
+					"case:escalation":"",
+					"case:followups":""
+				 }
+			  }
+		   }
+		}
+	});
+	
+	test("Get Activity Definition", 2, function () {
+        stop();
+        $.cordys['case'].getActivityDefinition("001CC42E-BB14-11E1-FA4E-3E43A945D4AF", {
+				beforeSend:function(xhr, settings){
+					var getActivityDefinitionRequest = "<SOAP:Envelope xmlns:SOAP='http://schemas.xmlsoap.org/soap/envelope/'><SOAP:Body><GetActivityDefinition xmlns='http://schemas.cordys.com/casemanagement/execution/1.0'><caseinstanceid>001CC42E-BB14-11E1-FA4E-3E43A945D4AF</caseinstanceid><activities><activityid></activityid></activities></GetActivityDefinition></SOAP:Body></SOAP:Envelope>";
+					equal(compareXML(getActivityDefinitionRequest,settings.data), true, "Compare Request XML");
+				},
+				success: function(data) {
+					equal(data.error, undefined, "Getting activity definition");
+					start();
+				},
+            error: function (jqXHR, errorStatus, errorThrown, errorCode, errorMessage) {
+                ok(false, "error found: " + errorMessage);
+                start();
+                return false;
+            }
+        })
+    });
+	
+	$.mockjax({
+		url: '*/com.eibus.web.soap.Gateway.wcp',
+		data: /GetActivityInstance/,
+		responseText: {
+		   tuple:{
+			  old:{
+				 ACTIVITY_INSTANCE:{
+					CASE_STATE:{
+					   "STATE_NAME":"Default State"
+					},
+					CASE_ACTIVITY:{
+					   "ACTIVITY_NAME":"Activity1",
+					   "ACTIVITY_TYPE":"HUMANTASK",
+					   "ACTIVITY_MODEL_ID":"001CC42E-BB14-11E1-FA4E-358C104134AF"
+					},
+					"ACTIVITY_INSTANCE_ID":"001CC42E-BB14-11E1-FA4E-3E43A94634AF",
+					"CASE_INSTANCE":"001CC42E-BB14-11E1-FA4E-3E43A945D4AF",
+					"ACTIVITY":"001CC42E-BB14-11E1-FA4E-3AD4CA6DF4AF",
+					"PLANNED_ON":"11342792264296",
+					"STARTED_ON":"11342792264296",
+					"LASTMODIFIED_ON":"11342792264375",
+					"LASTMODIFIED_BY":"cn=kvinoth,cn=organizational users,o=system,cn=cordys,cn=cu7bop4,o=vanenburg.com",
+					"CURRENT_STATUS":"RELEASED"
+				 }
+			  }
+		   }
+		}
+	});
+	
+	test("Get Activity Instance", 2, function () {
+        stop();
+        $.cordys['case'].getActivityInstance("001CC42E-BB14-11E1-FA4E-3E43A945D4AF", {
+				beforeSend:function(xhr, settings){
+					var getActivityInstanceRequest = "<SOAP:Envelope xmlns:SOAP='http://schemas.xmlsoap.org/soap/envelope/'><SOAP:Body><GetActivityInstance xmlns='http://schemas.cordys.com/casemanagement/execution/1.0'><caseinstanceid>001CC42E-BB14-11E1-FA4E-3E43A945D4AF</caseinstanceid><activityinstanceid></activityinstanceid></GetActivityInstance></SOAP:Body></SOAP:Envelope>";
+					equal(compareXML(getActivityInstanceRequest,settings.data), true, "Compare Request XML");
+				},
+				success: function(data) {
+					equal(data[0].CASE_ACTIVITY.ACTIVITY_NAME, "Activity1", "Activity instance name is Activity1");
+					start();
+				},
+            error: function (jqXHR, errorStatus, errorThrown, errorCode, errorMessage) {
+                ok(false, "error found: " + errorMessage);
+                start();
+                return false;
+            }
+        })
+    });
+	
+	$.mockjax({
+		url: '*/com.eibus.web.soap.Gateway.wcp',
+		data: /(\bCompleteActivityWithFollowup\b|\bCompleteActivity\b)/,
+		responseText:{
+		   activestates:{
+			  state:{
+				 "@name":"Default State",
+				 "@id":"RootCaseModelState",
+				 "text":"001CC42E-BB14-11E1-FA4E-3E43A94614AF"
+			  }
+		   }
+		}
+	});
+	
+	test("Complete Activity with Followup", 2, function () {
+        stop();
+        $.cordys['case'].completeActivityWithFollowup("001CC42E-BB14-11E1-FA4E-3E43A945D4AF", {
+				beforeSend:function(xhr, settings){				
+					var completeActivityWithFollowupRequest = "<SOAP:Envelope xmlns:SOAP='http://schemas.xmlsoap.org/soap/envelope/'><SOAP:Body><CompleteActivityWithFollowup xmlns='http://schemas.cordys.com/casemanagement/execution/1.0'><caseinstanceid>001CC42E-BB14-11E1-FA4E-3E43A945D4AF</caseinstanceid><activityinstanceid activityid=\"\"></activityinstanceid></CompleteActivityWithFollowup></SOAP:Body></SOAP:Envelope>";
+					equal(compareXML(completeActivityWithFollowupRequest,settings.data), true, "Compare Request XML");
+				},
+				success: function(data) {
+					equal(data.activestates.state.text, "001CC42E-BB14-11E1-FA4E-3E43A94614AF", "Complete activity with followup");
+					start();
+				},
+            error: function (jqXHR, errorStatus, errorThrown, errorCode, errorMessage) {
+                ok(false, "error found: " + errorMessage);
+                start();
+                return false;
+            }
+        })
+    });
+	
+	test("Complete Activity", 2, function () {
+        stop();
+        $.cordys['case'].completeActivity("001CC42E-BB14-11E1-FA4E-3E43A945D4AF", {
+				beforeSend:function(xhr, settings){				
+					var completeActivityRequest = "<SOAP:Envelope xmlns:SOAP='http://schemas.xmlsoap.org/soap/envelope/'><SOAP:Body><CompleteActivity xmlns='http://schemas.cordys.com/casemanagement/execution/1.0'><caseinstanceid>001CC42E-BB14-11E1-FA4E-3E43A945D4AF</caseinstanceid><activityinstanceid activityid=\"\"></activityinstanceid></CompleteActivity></SOAP:Body></SOAP:Envelope>";
+					equal(compareXML(completeActivityRequest,settings.data), true, "Compare Request XML");
+				},
+				success: function(data) {
+					equal(data.activestates.state.text, "001CC42E-BB14-11E1-FA4E-3E43A94614AF", "Complete activity");
+					start();
+				},
+            error: function (jqXHR, errorStatus, errorThrown, errorCode, errorMessage) {
+                ok(false, "error found: " + errorMessage);
+                start();
+                return false;
+            }
+        })
+    });
 
 })(window, jQuery)
