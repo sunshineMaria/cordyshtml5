dd4bec46d2ca813320d8b186c506ade136a7c1b6
sswathi <sswathi@cordys.com>
2012-08-13 06:23:42 +0000

    Added a separate test suite and test file for qunit tests for cordys.mobile apis which requires user interaction.

--- src/web/html5/test/test.cordys.mobile.js
+++ src/web/html5/test/test.cordys.mobile.js
@@ -1,86 +1,63 @@
 ﻿(function(window, $) {
 
 	module("Mobile Plugin Test");
+	
+	/*
+	
+	The following tests are covered under model plugin test
+		1. Notification: Beep
+		2. Notification: Vibrate
+		3. fileReader: Read As Data URL (Prerequisite: A picture '1.jpg' in location /sdcard/DCIM/Camera in the device )
+		4. fileReader: Read As Text (Prerequisite: A text file 'NativeAppTest.txt' in location /sdcard/download/ in the device )
 
+	
+	*/
+
+//1. Notification: Beep
 	test("Notification Beep", 0, function(){
 		stop();
 		$.cordys.mobile.notification.beep(1);
 		start();
 	});
-	/*
+
+//2. Notification: Vibrate
 	test("Notification Vibrate", 0, function(){
 		stop();
 		$.cordys.mobile.notification.vibrate(2000);
 		start();
 	});
-	*/
-	test("Notification Alert", 1, function(){
-		$.cordys.mobile.notification.alert("Alert Notification Test", function(){
-			console.log('success callback in alert callback');
-			equal(true,true);
-			start();
-		},
-		"Alert Notification Test", "OK");
-		stop();
-	});
-/*	
-
-	test("Notification Confirm", 0, function(){
-		stop();
-		$.cordys.mobile.notification.confirm('Confirm Notification Test',function(){
-			start();
-			console.log('success callback in confirm callback');
-		},'Cofirm Notification Test','Yes,No');
-		
-	});
-
 	
-	test("fileReader Read As Data URL", 1, function(){
+//3. fileReader: Read As Data URL
+	test("fileReader Read As Data URL", 2, function(){
 		stop();
 		$.cordys.mobile.fileReader.readAsDataURL('file:///sdcard/DCIM/Camera/1.jpg', 
 			function(data){
 				start();
-				notEqual(data,undefined);
-				//alert("inside fileReader Read As Data URL success callback");
+				ok(true, "inside fileReader Read As Data URL success callback");
+				notEqual(data, undefined, "file read as Data URL: Successful");
 			}, 	
 			function(data){
 				start();
-				equal(data,undefined);
-				//alert("inside fileReader Read As Data URL error callback");
+				ok(false,"inside fileReader Read As Data URL error callback");
+				equal(data, undefined, "file read as Data URL: Failed");
 		});
 	});
 	
-	test("fileReader Read As Text", 1, function(){
+//4. fileReader: Read As Text
+	test("fileReader Read As Text", 2, function(){
 		stop();
 		$.cordys.mobile.fileReader.readAsDataURL('file:///sdcard/download/NativeAppTest.txt', 
 			function(data){
 				start();
-				equal(data,"data:text/plain;base64,dGVzdA==");
-				//alert("inside fileReader Read As Text success callback");
+				ok(true, "inside fileReader Read As Text success callback");
+				equal(data, "data:text/plain;base64,dGVzdA==", "file read as Text: Successful");
 			}, 	
 			function(data){
 				start();
-				equal(data,undefined);
-				//alert("inside fileReader Read As text error callback");
+				ok(false, "inside fileReader Read As text error callback");
+				ok(false, "file read as Text: Failed");
 		});
 	});
 	
-	
-
-	test("camera getPicture", 1, function(){
-		stop();
-		$.cordys.mobile.camera.getPicture(function(parameters) {
-			start();
-			notEqual(parameters.imageData,undefined);
-			alert("Photo taken");
-		}, function(parameters) {
-			start();
-			alert("failed: " + parameters.message);
-		}, {
-			quality: 50, 
-			destinationType: $.cordys.mobile.camera.DestinationType.DATA_URL
-		});
-	});
-*/
 })(window, jQuery)
 	
