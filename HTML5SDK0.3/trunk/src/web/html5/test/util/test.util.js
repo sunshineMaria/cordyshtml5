bf5cdc88fcdc6690726b9d8819e8948e5346863a
csharbel@cordys.com <csharbel@cordys.com@f061221c-096d-faed-b717-863d521ff404>
2012-10-03 08:11:47 +0000

    Deleting as the tag as it was created in the wrong location. Moved this already to the tags folder

--- HTML5SDK0.3/trunk/src/web/html5/test/util/test.util.js
+++
@@ -1,6 +0,0 @@
-function compareXML(sourceXML, targetXML)
-{
-	sourceXMLDoc = $.parseXML(sourceXML);
-	targetXMLDoc = $.parseXML(targetXML);	
-	return ($.cordys.json.xml2jsstring(sourceXMLDoc) === $.cordys.json.xml2jsstring(targetXMLDoc));
-}
