6cbdf9a97f71eb0c4c5081d17befc79c49752651
kvinoth@cordys.com <kvinoth@cordys.com@f061221c-096d-faed-b717-863d521ff404>
2012-10-08 04:56:23 +0000

    Added Some Comments

--- branches/nativeapp/assets/www/js/view_model/delete_instance.js
+++ branches/nativeapp/assets/www/js/view_model/delete_instance.js
@@ -8,7 +8,9 @@ function DeleteInstanceViewModel(parentModel) {
 	
 	this.onSubmitDeleteServer = function(server) {
 		parentModel.instances.remove(server);
+		// delete the cookies so that we do not have a problem if we add it back
 		server.deleteCookies();
+		// move back to the server list
 		$.mobile.changePage('#');
 	};
 	
