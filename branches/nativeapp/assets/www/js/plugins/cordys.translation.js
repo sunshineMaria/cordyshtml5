a606c3345a657728f20893a5df946fa07d70c188
pkruysse@cordys.com <pkruysse@cordys.com@f061221c-096d-faed-b717-863d521ff404>
2012-12-07 08:54:13 +0000

    Corrected preference of language

--- branches/nativeapp/assets/www/js/plugins/cordys.translation.js
+++ branches/nativeapp/assets/www/js/plugins/cordys.translation.js
@@ -40,9 +40,8 @@
 	}
 
 	var getRuntimeLanguage = function(language) {
-		window._$DefLanguage = window._$DefLanguage || $.Deferred();
-		language = language || window.navigator.language;
-		if (typeof(getURLParameter) != "undefined") language = getURLParameter(window.location, "language");
+		var _$DefLanguage = $.Deferred();
+		language = language || (typeof(getURLParameter) != "undefined" ? getURLParameter(window.location, "language") : "") || window.navigator.language;
 		if (!language) {
 			if ($.cordys.isMobile && $.cordys.mobile && $.cordys.mobile.globalization) {
 				$.cordys.mobile.globalization.getLocaleName().done(function(result){
